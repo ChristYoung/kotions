f95c791b69ae0fd4e77871bdd34e3bd06df77cc6
jay.yang <858851768@qq.com>
2023-12-17 17:28:25 +0800

    chore: need to watch SideBar character

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import { Spinner } from '@/components/spinner';
 import { Button } from '@/components/ui/button';
+import { useConvexAuth } from 'convex/react';
 import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
 export const Heading: React.FC = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     return <div className='__Heading max-w-3xl space-y-4'>
         <h1 className='text-3xl sm:text-5xl md:test-6xl font-bold'>
             Your Ideas, Documents, and Plans, Unified. Welcome to <span className='underline'>Kotion</span>
@@ -11,6 +15,17 @@ export const Heading: React.FC = () => {
         <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
             Kotion is the connected workspace where <br /> better, faster work happens.
         </h3>
-        <Button>Enter Kotion <ArrowRight className='h-4 w-4 ml-2' /></Button>
+        { isLoading && (
+            <div className='w-full flex items-center justify-center text-center'>
+                <Spinner size='lg' />
+            </div>
+        ) }
+        { isAuthenticated && !isLoading && (
+            <Button asChild>
+                <Link href="/documents">
+                    Enter Kotion <ArrowRight className='h-4 w-4 ml-2' />
+                </Link>
+            </Button>
+        )}
     </div>;
 };
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,15 +1,44 @@
 "use client"
 
 import { ModeToggle } from '@/components/mode-toggle';
+import { Spinner } from '@/components/spinner';
+import { Button } from '@/components/ui/button';
 import { useScrollTop } from '@/hooks/useScrollTop';
 import { cn } from '@/lib/utils';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import { useConvexAuth } from 'convex/react';
+import Link from 'next/link';
 
 export const Navbar: React.FC = () => {
     const scrolled = useScrollTop();
+    const { isAuthenticated, isLoading } = useConvexAuth();
     return <div className={cn('z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6',
     scrolled && 'border-b shadow-sm')}>
         <div className='font-bold'>Kotion</div>
         <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
+            {isLoading && (
+                <Spinner />
+            )}
+            {!isAuthenticated && !isLoading && (
+                <>
+                    <SignInButton mode='modal'>
+                        <Button variant="ghost" size="sm">Log In</Button>
+                    </SignInButton>
+                    <SignInButton mode='modal'>
+                        <Button size="sm">Get Kotion free</Button>
+                    </SignInButton>
+                </>
+            )}
+            {
+                isAuthenticated && !isLoading && (
+                    <>
+                        <Button size="sm" variant="ghost" asChild>
+                            <Link href="/documents">Enter Kotion</Link>
+                        </Button>
+                        <UserButton afterSignOutUrl='/' />
+                    </>
+                )
+            }
             <ModeToggle />
         </div>
     </div>;
