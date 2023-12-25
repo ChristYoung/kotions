1cb6459b045ef0a94be61522b064a144f4196198
jay.yang <858851768@qq.com>
2023-12-25 17:21:55 +0800

    chore:

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,19 +1,59 @@
 "use client";
 
+import { ChevronsLeft, MenuIcon } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import { ElementRef, useRef, useState } from 'react';
+import { useMediaQuery } from 'usehooks-ts';
+import { cn } from '@/lib/utils'
+
 const Navigation: React.FC = () => {
+    const isMobile = useMediaQuery('(max-width: 768px)');
+    const pathName = usePathname();
+
+    const isResizingRef = useRef(false);
+    const sideBarRef = useRef<ElementRef<'aside'>>(null);
+    const navBarRef = useRef<ElementRef<'div'>>(null);
+    const [isResetting, setIsResetting] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+    const handleMouseDown = () => {};
+    const handleMouseClick = () => {};
+
     return <>
-        <aside className='group h-full bg-secondary
-        overflow-y-auto relative flex w-60 flex-col z-[99999]'>
+        <aside ref={sideBarRef} className={cn(
+                "group h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+                isResetting && "transition-all ease-in-out duration-300",
+                isMobile && "w-0",
+            )}>
+            <div role='button' className={cn(
+                    "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover:opacity-100 transition",
+                    isMobile && "opacity-100",
+                )}>
+                    <ChevronsLeft className='h-6 w-6' />
+            </div>
             <div>
                 <p>Action items</p>
             </div>
             <div className='mt-4'>
-                <p>Doc</p>
+                <p>isMobile: {JSON.stringify(isMobile)}</p>
             </div>
-            <div className='opacity-0 group-hover:opacity-100
-            transition cursor-ew-resize
-            absolute h-full w-1 bg-primary/10 right-0 top-0' />
+            {/* Hover over the side bar to expand or collapse the aside. */}
+            <div 
+                onMouseDown={() => {}}
+                onClick={() => {}}
+                className='opacity-0 group-hover:opacity-100
+                transition cursor-ew-resize
+                absolute h-full w-1 bg-primary/10 right-0 top-0' />
         </aside>
+        <div ref={navBarRef} className={cn(
+            "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+            isResetting && "transition-all ease-in-out duration-300",
+            isMobile && "left-0 w-full",
+        )}>
+            <nav className='bg-transparent px-3 py-2 w-full'>
+                {isCollapsed && <MenuIcon role='button' className='h-6 w-6 text-muted-foreground'></MenuIcon>}
+            </nav>
+        </div>
     </>;
 };
 
