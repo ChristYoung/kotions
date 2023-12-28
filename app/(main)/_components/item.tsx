65d6d48db67c85c8dd490fa14e969c35d1b8e5e6
jay.yang <858851768@qq.com>
2023-12-28 15:38:42 +0800

    chore:

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,22 +1,50 @@
 "use client"
 
-import { LucideIcon } from 'lucide-react';
+import { Id } from '@/convex/_generated/dataModel';
+import { cn } from '@/lib/utils';
+import { LucideIcon, ChevronDown, ChevronRight } from 'lucide-react';
 
 export interface ItemProps extends React.HTMLAttributes<HTMLDivElement> {
-    id?: string;
+    id?: Id<'documents'>;
+    documentIcon?: string;
+    active?: boolean;
+    expanded?: boolean;
+    isSearch?: boolean;
+    level?: number;
     label?: string;
-    icon?: LucideIcon;
+    icon: LucideIcon;
+    onExpand?: () => void;
 }
 
 export const Item: React.FC<ItemProps> = (props: ItemProps) => {
-    const { label, icon: Icon, onClick } = props;
-    return <div onClick={(e) => onClick && onClick(e)} role='button' className='__item group min-h-[27px] cursor-pointer text-sm
-                py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium'
-                style={{paddingLeft: '12px'}}
+    const { label, icon: Icon, onClick, active, id,
+        documentIcon, level = 0, isSearch, expanded, onExpand } = props;
+    
+    const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
+    return <div onClick={(e) => onClick && onClick(e)} role='button'
+                className={cn(
+                    "__item group min-h-[27px] cursor-pointer text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+                    active && 'bg-primary/5 text-primary',
+                )}
+                style={{paddingLeft: level ? `${(level * 12) + 12}px` : '12px'}}
             >
-                { Icon && <Icon className='shrink-0 h-[18px] mr-2 text-muted-foreground' /> }
+                {!!id && (
+                    <div role='button' className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
+                    onClick={() => {}}>
+                        <ChevronIcon className='h-4 w-4 shrink-0 text-muted-foreground/50' />
+                    </div>
+                )}
+                {documentIcon ? (
+                    <div className='shrink-0 mr-2 text-[18px]'>{documentIcon}</div>
+                ) : <Icon className='shrink-0 h-[18px] mr-2 text-muted-foreground' />}
                 <span className='truncate'>
-                {label}
+                    {label}
                 </span>
+                {isSearch && (
+                    <kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px=1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
+                        <span className='text-xs'>⌘</span>k
+                    </kbd>
+                )}
             </div>;
 };
