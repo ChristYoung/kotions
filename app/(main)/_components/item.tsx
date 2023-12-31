e1d52a7f356dab6ee378f2c352319d271a8422ee
jay.yang <858851768@qq.com>
2023-12-31 20:17:06 +0800

    chore: archive documents

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { cn } from '@/lib/utils';
+import { useUser } from '@clerk/clerk-react';
 import { useMutation } from 'convex/react';
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from 'lucide-react';
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
@@ -26,12 +28,25 @@ export const Item = (props: ItemProps) => {
         documentIcon, level = 0, isSearch, expanded, onExpand } = props;
     
     const create = useMutation(api.documents.create);
+    const archive = useMutation(api.documents.archive);
+    const { user } = useUser();
     const router = useRouter();
 
     const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.stopPropagation();
         e.preventDefault();
         onExpand?.();
+    };
+
+    const onArchived = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+        if (!id) return;
+        const archivePromise = archive({ id });
+        toast.promise(archivePromise, {
+            loading: 'Archiving...',
+            success: 'Archived',
+            error: 'Failed to archive'
+        });
     };
 
     const onCreate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -83,6 +98,22 @@ export const Item = (props: ItemProps) => {
                 {
                     !!id && (
                         <div className='ml-auto flex items-center gap-x-2'>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
+                                    <div role='button' className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'>
+                                        <MoreHorizontal className='h-4 w-4 text-muted-foreground'></MoreHorizontal>
+                                    </div>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align='start' className='w-60' side='right' forceMount>
+                                    <DropdownMenuItem onClick={onArchived}>
+                                        <Trash className='h-4 w-4 mr-2' /> Delete
+                                    </DropdownMenuItem>
+                                    <DropdownMenuSeparator />
+                                    <div className='text-xs text-muted-foreground p-2'>
+                                        Last edited by: {user?.fullName}
+                                    </div>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                             <div 
                                 role='button'
                                 onClick={onCreate}
--- convex/documents.ts
+++ convex/documents.ts
@@ -2,6 +2,47 @@ import { v } from 'convex/values';
 import { mutation, query } from './_generated/server';
 import { Doc, Id } from './_generated/dataModel';
 
+export const archive = mutation({
+    args: { id: v.id('documents') }, // the id of the document which we want to delete.
+    handler:async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error('You must be logged in to create a document');
+        }
+        const userId = identity.subject;
+        const existingDoc = await ctx.db.get(args.id);
+        if (!existingDoc) {
+            throw new Error('Document not found');
+        }
+
+        if (existingDoc.userId !== userId) {
+            throw new Error('Unauthorized');
+        }
+
+        // 当删除一个父文档的时候, 需要递归删除子文档
+        const recursiveArchive = async (docId: Id<'documents'>) => {
+            const children = await ctx.db.query('documents')
+            .withIndex('by_user_parent', (q) => q.eq('userId', userId).eq('parentDocument', docId))
+            .collect();
+        
+            for (const child of children) {
+                await ctx.db.patch(child._id, {
+                    isArchived: true
+                });
+                await recursiveArchive(child._id);
+            }
+        };
+
+        const document = await ctx.db.patch(args.id, {
+            isArchived: true,
+        });
+
+        recursiveArchive(args.id);
+        
+        return document;
+    }
+});
+
 export const getSidebar = query({
     args: {
         parentDocument: v.optional(v.id('documents'))
